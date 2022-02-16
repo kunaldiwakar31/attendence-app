ccd575f1eba972651ca45b319768ae8bda14a83f
Arshdeep Singh <arsh76278@gmail.com>
2022-02-16 16:05:20 +0530

    Modified UI elements

--- lib/joinClass.dart
+++ lib/joinClass.dart
@@ -11,32 +11,48 @@ class JoinClass extends StatelessWidget {
         title: const Text("Join Class"),
       ),
       backgroundColor: const Color.fromARGB(255, 37, 130, 236),
-      body: Container(
-          child: Column(
-        children: const <Widget>[
-          SizedBox(
-            height: 20.0,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Class Code',
-              fillColor: Colors.white,
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
-              focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(onPressed: null, child: Text("Join Class")),
-        ],
-      )),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Class Code',
+                fillColor: Colors.white,
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              ),
+            ),
+            const SizedBox(
+              height: 22.0,
+            ),
+            ElevatedButton(
+              onPressed: null,
+              onHover: (value) {},
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Join Class',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
