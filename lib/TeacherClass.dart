ad432755f950d5647e1441e45979d5527cb6f607
Arshdeep Singh <arsh76278@gmail.com>
2022-02-26 16:14:46 +0530

    Added Route to manage particular class

--- lib/TeacherClass.dart
+++ lib/TeacherClass.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:attendence_app/TeacherClassRoutes/StudentList.dart';
 
 class TeacherClass extends StatefulWidget {
   final uid;
@@ -12,20 +13,6 @@ class _TeacherClassState extends State<TeacherClass> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Register',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Students',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -35,6 +22,18 @@ class _TeacherClassState extends State<TeacherClass> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _widgetOptions = <Widget>[
+      const Text(
+        'Register',
+        style: optionStyle,
+      ),
+      StudentList(uid: widget.uid),
+      const Text(
+        'Manage',
+        style: optionStyle,
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('BottomNavigationBar Sample'),
@@ -53,9 +52,9 @@ class _TeacherClassState extends State<TeacherClass> {
             label: 'Students',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
+            icon: Icon(Icons.settings),
+            label: 'Manage',
+          )
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
